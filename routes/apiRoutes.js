6d3bfc6012ebaed9bab62a2dd8bfc6045a78ac7e
JDLong <jdlong1980@gmail.com>
2019-03-16 23:11:14 -0400

    updated main, added materialize and other minor adjustments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,13 @@
 var db = require("../models");
 
 module.exports = function(app) {
-
+    app.post("/api/register", function(req, res) {
+        //passport encyrption
+        db.User.create({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            password:
+        })
+    })
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,6 +7,11 @@ module.exports = function(app) {
     });
 
     app.get("/barcodes", function(req, res) {
-        res.render("barcodes");
-    })
+        if (req.query.barcode) {
+            //find one from database to get item name, description, photo
+            res.render("scanned", { barcode: req.query.barcode });
+        } else {
+            res.render("barcodes");
+        };
+    });
 };
