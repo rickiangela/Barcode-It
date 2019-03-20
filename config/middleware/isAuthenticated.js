87b8bb880900e51d51d4fa923edf51f66ea100c9
BBadoo <bbader0806@gmail.com>
2019-03-20 11:58:57 -0400

    passport working

--- config/middleware/isAuthenticated.js
+++ config/middleware/isAuthenticated.js
@@ -2,10 +2,12 @@
 module.exports = function(req, res, next) {
 
     if (req.user) {
+      console.log("user authenticated")
       return next();
+      
     }
   
-
+    console.log("user not authenticated")
     return res.redirect("/");
   };
   
