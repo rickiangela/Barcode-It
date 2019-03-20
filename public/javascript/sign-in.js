f467eee56971a9e3e2f7f289f27d63e0ac0d7fc1
JDLong <jdlong1980@gmail.com>
2019-03-20 14:58:06 -0400

    updated signout and routes

---
+++ public/javascript/sign-in.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    // Getting references to our form and inputs
+    var emailInput = $("#email-input");
+    var passwordInput = $("#password-input");
+
+    // When the form is submitted, we validate there's an email and password entered
+    $("#signinSubmit").on("click", function(event) {
+        event.preventDefault();
+        var userData = {
+            email: emailInput.val().trim(),
+            password: passwordInput.val().trim()
+        };
+
+        if (!userData.email || !userData.password) {
+            return;
+        }
+
+        // If we have an email and password we run the loginUser function and clear the form
+        loginUser(userData.email, userData.password);
+        emailInput.val("");
+        passwordInput.val("");
+    });
+
+    // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+    function loginUser(email, password) {
+        $.post("/api/signin", {
+            email: email,
+            password: password
+        }).then(function(data) {
+            window.location.replace(data);
+            // If there's an error, log the error
+        });
+    };
+
+});
